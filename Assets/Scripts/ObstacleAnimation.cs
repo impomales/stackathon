41338391d550fe86d5f651b21dc0de0f8857a474
Isaias Pomales <impomales@gmail.com>
2018-06-14 17:55:01 -0400

    obtacles moving

---
+++ Assets/Scripts/ObstacleAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleAnimation : MonoBehaviour {
+	public float speed;
+	public Vector3 start;
+	public bool toward;
+	private float step;
+
+	// Update is called once per frame
+	void Update () {
+		if (toward && transform.position.x > 	12) transform.Translate(new Vector3(-24, 0, 0));
+		else if (!toward && transform.position.x < -12) transform.Translate(new Vector3(24, 0, 0));
+		step = speed * Time.deltaTime;
+		if (!toward) step *= -1;
+		transform.Translate(new Vector3(step, 0, 0));
+	}
+}
