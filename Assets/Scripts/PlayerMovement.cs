b1675552b8c2f020ab8e0a827f99dc3197ec6ddf
Isaias Pomales <impomales@gmail.com>
2018-06-14 14:47:27 -0400

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		rb.MovePosition(transform.position + new Vector3(moveHorizontal * 50, 0, moveVertical) * Time.deltaTime);
+	}
+}
